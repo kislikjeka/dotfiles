c4abe9e64f9450df5a8d7016e7e1702ca59ea977
Evgenii Kislichenko <evgenii.kislichenko@indriver.com>
2024-08-29 15:49:49 +0300

    Add harpoon mappings

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,37 +1,48 @@
 return {
   'folke/trouble.nvim',
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  config = function()
+    require('trouble').setup {
+      focus = true,
+      auto_jump = false,
+      auto_close = true,
+      warn_no_results = false,
+      preview = {
+        type = 'split',
+        relative = 'win',
+        size = 0.5,
+        fixed = true,
+        position = 'right',
+        scratch = false,
+      },
+      modes = {
+        symbols = {
+          auto_preview = false,
+          win = {
+            size = 0.3,
+          },
+          preview = {
+            position = 'left',
+          },
+        },
+      },
+    }
+  end,
   cmd = 'Trouble',
   keys = {
     {
-      '<leader>xx',
+      '<leader>xX',
       '<cmd>Trouble diagnostics toggle<cr>',
       desc = 'Diagnostics (Trouble)',
     },
     {
-      '<leader>xX',
+      '<leader>xx',
       '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
       desc = 'Buffer Diagnostics (Trouble)',
     },
     {
-      '<leader>cs',
+      '<leader>ds',
       '<cmd>Trouble symbols toggle focus=false<cr>',
       desc = 'Symbols (Trouble)',
-    },
-    {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-      '<leader>xL',
-      '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-    },
-    {
-      '<leader>xQ',
-      '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
     },
   },
 }
