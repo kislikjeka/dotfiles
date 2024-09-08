dd02f1eae2886d84b1ae0a20611c40c35f8924f8
Evgenii Kislichenko <evgenii.kislichenko@indriver.com>
2024-09-08 16:13:16 +0300

    Add home-manager

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,20 +1,28 @@
 {
   description = "Example Darwin system flake";
 
-  inputs = {
+inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+      environment = {
+      systemPackages = with pkgs; [ 
+        vim
+        eza #ls
+        direnv
+      ];
+    };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -36,13 +44,22 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+      users.users.evgenijkislicenko.home = "/Users/evgenijkislicenko";
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Evgenijs-MacBook-Pro
     darwinConfigurations."Evgenijs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      system = "aarch64-darwin";
+      modules = [ 
+	configuration
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.evgenijkislicenko = import ./home.nix;
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
---
+++ nix-darwin/home.nix
@@ -0,0 +1,54 @@
+# home.nix
+# home-manager switch 
+
+{ config, pkgs, ... }:
+
+{
+  home.username = "evgenijkislicenko";
+  home.homeDirectory = "/Users/evgenijkislicenko";
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+# Makes sense for user specific applications that shouldn't be available system-wide
+  home.packages = [
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".zshrc".source = ~/dotfiles/.zshrc;
+    ".config/wezterm".source = ~/dotfiles/wezterm;
+    ".config/starship".source = ~/dotfiles/starship;
+    ".config/nvim".source = ~/dotfiles/nvim;
+    ".config/nix".source = ~/dotfiles/nix;
+    ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    ".config/tmux".source = ~/dotfiles/tmux;
+    ".config/lazygit".source = ~/dotfiles/lazygit;
+    ".config/fish".source = ~/dotfiles/fish;
+    ".config/k9s".source = ~/dotfiles/k9s;
+  };
+
+  home.sessionVariables = {
+  };
+
+  home.sessionPath = [
+    "/run/current-system/sw/bin"
+      "$HOME/.nix-profile/bin"
+  ];
+
+  programs = {
+      fzf.enable = true;
+      home-manager.enable = true;
+      starship.enable = true;
+      zoxide.enable = true;
+      zsh = {
+        enable = true;
+        initExtra = ''
+          # Add any additional configurations here
+          export PATH=/run/current-system/sw/bin:$HOME/.nix-profile/bin:$PATH
+          if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+            . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+          fi
+        '';
+      };
+  };
+}
